a3d37b7bdbd7f772bfddeaf84c74b48aa5d0f613
Keegan <juan789huarcaya@gmail.com>
2024-04-25 19:52:07 -0500

    Mejorando el codigo de la autenticacion de usuarios

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -3,16 +3,30 @@ import bcrypt from "bcrypt";
 
 export const checkAccount = async (req, res) => {
   const { username, password } = req.body;
-  const [result] = await pool.query(`SELECT * FROM users WHERE username = ?`, [
-    username,
-  ]);
+  const [checkUser] = await pool.query(
+    `SELECT * FROM users WHERE username = ?`,
+    [username]
+  );
 
-  const user = result[0];
-  const confirmation = await bcrypt.compare(password, user.password);
+  if (checkUser.length > 0) {
+    const user = checkUser[0];
+    const confirmation = await bcrypt.compare(password, user.password);
+    if (confirmation) {
+      const user_detail_id = user.user_detail_id;
+      const [user_detail] = await pool.query(
+        "SELECT * FROM users_details WHERE user_detail_id = ?",
+        [user_detail_id]
+      );
 
-  if (confirmation) {
-    return res.status(200).json({ message: "Inicio de sesion exitoso" });
-  } else {
-    return res.status(401).json({ message: "Credenciales incorrectas" });
+      return res.status(200).json({
+        message: "Inicio de sesion exitoso",
+        username: user.username,
+        email: user_detail[0].email,
+        credential: user.credential,
+        name: user_detail[0].first_name,
+        last_name: user_detail[0].last_name,
+      });
+    }
   }
+  return res.status(401).json({ message: "Credenciales incorrectas" });
 };
